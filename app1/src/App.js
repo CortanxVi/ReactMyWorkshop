49e8a879f217ec7fbde1f7d644616b138a31b065
CortanxVi <JirayuP@outlook.com>
2026-07-13 10:56:07 +0700

    app.js practice jsx

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  let r = 'React'
+  let rn = 'React Native'
+  const el =(
+    <div>
+      Create Website & Mobile Apps<br />
+      with {r} & {rn}
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <img src={logo} alt="this is logoEiEi" /> */}
+      <h1>Hello world <br />My name is Brian</h1>
+      <h3>{el}</h3>
     </div>
   );
 }
