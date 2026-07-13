afd084f97ba2ce26a679bb5ff8969b6231e53dc8
CortanxVi <JirayuP@outlook.com>
2026-07-13 11:16:47 +0700

    Update App.js and create style.css

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,29 +1,12 @@
 import logo from './logo.svg';
-import {useState} from 'react';
-import './App.css';
+import './style.css';
 
 function App() {
-  let r = 'React'
-  let rn = 'React Native'
-  const el =(
-    <div>
-      Create Website & Mobile Apps<br />
-      with {r} & {rn}
-    </div>
-  );
-
-  const h1Style = {
-    backgroundColor: 'cyan',
-    textAlign: 'Center',
-    color: 'red',
-    fontSize: 'larger',
-    padding: '3px',
-  };
-
   return (
-    <div>
-      <h1 style={h1Style}>Hello React</h1>
-    </div>
+    <>
+      <h2>DeveloperThai.com</h2>
+      <div className='title'>React & React Native</div>
+    </>
   );
 }
 
