5f05aa6e21a120de92c694c59bb66c2b15dae3da
CortanxVi <JirayuP@outlook.com>
2026-07-20 10:35:42 +0700

    create components to display nad try it out.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './style.css';
+import React from 'react';
+import { Header, Content, Footer } from './components/func-components';
 
 function App() {
   return (
-    <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
-    </>
+      [<Header />,
+      <Content />,
+      <Footer />]
   );
 }
 
