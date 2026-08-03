601d93cc25a593676608230d736f7dc939964162
CortanxVi <JirayuP@outlook.com>
2026-08-03 10:11:23 +0700

    Learning to create Event Handler with Func Comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Header, Content, Footer } from './components/func-components';
-import Calendar from './components/class-components';
+import { Calculator2 } from './calculator';
 
 function App() {
   return ( 
-      [<Header />,
-      <p><center><Calendar /></center></p>,
-      <Content />,
-      <Footer />]
+    <Calculator2 />
   );
 }
 
