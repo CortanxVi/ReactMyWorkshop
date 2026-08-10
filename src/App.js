9093f691b2881a9c10ecf25d418a30a98e923f9b
CortanxVi <JirayuP@outlook.com>
2026-08-10 11:23:14 +0700

    useState Function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Calculator2 } from './calculator';
 import { EventData2 } from './event-data';
 import RefsFunc from './refs-func';
 import RefsArray from './refs-array';
+import StateFunc from './state-func';
 
 function App() {
   return ( 
-    <RefsArray/>
+    <StateFunc/>
   );
 }
 
