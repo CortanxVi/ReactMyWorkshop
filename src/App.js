a1eda43f86f4892b22d8d460dd9f4fa31d7ea991
CortanxVi <JirayuP@outlook.com>
2026-09-07 11:01:37 +0700

    react-router-dom practice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React from 'react'
-import {userContext} from './context'
-import Header2 from './context-header2'
-import Context2 from './context-content2'
+import React from "react";
+import {userContext} from "./context"
+import Router3 from "./router3";
 
-export default function App(){
-  let [user, setUser] = React.useState('')
+function App() {
   return (
-    <userContext.Provider value={[user, setUser]}>
-      <Header2 />
-      <Content2 />
-    </userContext.Provider>
-  )
+    <Router3/>
+  );
 }
+export default App;
